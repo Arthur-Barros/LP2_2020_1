8e9bbe7ea2208c831b52e43ed9e93df05c3e00c0
Arthur Barros <barrosarthur773@gmail.com>
2020-03-17 22:49:18 -0400

    Versão 2.1

--- aula02/src/index.js
+++ aula02/src/index.js
@@ -1,8 +1,5 @@
 import ArrecadacaoDAO from './model/dao/ArrecadacaoDAO';
 
-
 let dao = new ArrecadacaoDAO();
 
-let municipios = dao.carregarArrecadacoesDosMunicipios();
-
-console.log(municipios);
+dao.carregarArrecadacoesPorAno(2000);
--- aula02/src/model/dao/ArrecadacaoDAO.js
+++ aula02/src/model/dao/ArrecadacaoDAO.js
@@ -1,14 +1,14 @@
 //sabe ler o arquivo esse fs  e o path mostra o fs  
 import fs from 'fs';
 import path from 'path';
-import  excelToJson from 'convert-excel-to-json';
+import excelToJson from 'convert-excel-to-json';
 
 import Municipio from '../entity/Municipio';
+import { ANOS } from "../../constants/Coluna";
 
+export default class ArrecadacaoDAO {
 
-export default class ArrecadacaoDAO{
-
-    carregarArrecadacoesDosMunicipios (){
+    carregarArrecadacoesDosMunicipios() {
         let municipios = [];
 
 
@@ -16,14 +16,10 @@ export default class ArrecadacaoDAO{
          * Aponta para o arquivo da planilha de dados
          * 
          */
-        let caminhoArquivo = path.resolve(__dirname,'../../data/dados.xls');
+       
 
 
-        let dados = excelToJson({
-            source: fs.readFileSync(caminhoArquivo),
-            // intervalo das células do excel  - se colocar nada ele assuma que é a primeira planilha
-            range: 'A4:P185'
-        });
+        let dados = this.extrairDadosDaPlanilha('A4:P185');
 
         dados.Plan1.forEach(elemento => {
             let nome = elemento.A;
@@ -35,7 +31,7 @@ export default class ArrecadacaoDAO{
              */
             let valores = Object.values(elemento);
             let arrecadacoes = valores.slice(1, valores.length);
-            let municipio = new Municipio (nome, arrecadacoes);
+            let municipio = new Municipio(nome, arrecadacoes);
             municipios.push(municipio);
 
         })
@@ -44,5 +40,30 @@ export default class ArrecadacaoDAO{
         return municipios;
     }
 
+    carregarArrecadacoesPorAno(ano) {
+        if (ano in ANOS) {
+            let coluna = ANOS[ano];
+            let range = `${coluna}4:${coluna}185`;
 
+            let arrecadacoes = this.extrairDadosDaPlanilha(range);
+            let nomesMunicipios = this.extrairDadosDaPlanilha('A:4A185');
+            console.log(arrecadacoes, nomesMunicipios);
+        }
+    }
+
+    extrairDadosDaPlanilha(range) {
+        /**
+         * Aponta para o arquivo da planilha de dados
+         */
+        let caminhoArquivo = path.resolve(__dirname, '../../data/dados.xls');
+
+        let dados = excelToJson({
+            source: fs.readFileSync(caminhoArquivo),
+            //A intervalo das células do exvel - se não colocar nada ele assume que é a primeira planilha 
+            // A linha abaixo é isso range: range
+            range
+        });
+
+        return dados;
+    }
 }
